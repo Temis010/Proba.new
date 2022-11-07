849dc3e853350e983f3828e49f221a415d0b020a
Artem Sitnikov <artemsitnikov13@rambler.ru>
2022-11-07 14:12:45 +0300

    Окончательная версия второй лабы

--- lab-inf1-tren/Checking.cpp
+++ lab-inf1-tren/Checking.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-	float a, b, c;
-	a = 1;
-	a = 2.0 / 3;
-	b = a + 90;
-	c = 34.0 * 87;
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;
-	cout << "END!!!";
+	float a, b, c, x, y, z, otv;
+	a = (5 + 1.0 / 2) / 2.2 + 6 + 2.0 / 3;
+	b = 0.75 + 4 + 1.0 / 4 - 0.8 * (6 + 2.0 / 3);
+	x = (4 + 1.0 / 3 + 9) * 0.75;
+	y = 6.25 - (3 + 3.0 / 4);
+	c = (a / b) / (1 + 3.0 / 7);
+	z = x / y;
+	otv = c - z;
+	cout << "Otvet: " << otv;	
 	return 0;
 }
